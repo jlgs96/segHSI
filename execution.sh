8b2298bd648f6416a14076a403a37b58086ca38c
jlgs96 <joselgs96@gmail.com>
2021-02-11 19:19:00 +0100

    Add files via upload

--- execution.sh
+++ execution.sh
@@ -1,39 +1,39 @@
 #!/bin/sh
-
+# 
 # for i in 0 1 2 3 4
-for i in 3 4
-do
-    rm ./savedmodels/*
-    python -u train.py --network_arch unet --use_augs --network_weights_path ./savedmodels/Unet_normal.pt --idtest $i
-    python -u test.py --network_arch unet --network_weights_path ./savedmodels/Unet_normal.pt --idtest $i
-
-    python -u train.py --network_arch unet --use_augs --boxdown --network_weights_path ./savedmodels/Unet_normal_boxconv.pt --idtest $i
-    python -u test.py --network_arch unet --boxdown --network_weights_path ./savedmodels/Unet_normal_boxconv.pt --idtest $i
-
-
-    python -u train.py --network_arch unet --use_augs --use_SE --use_preluSE --network_weights_path ./savedmodels/Unet__SE_PRELU.pt --idtest $i
-    python -u test.py --network_arch unet  --use_SE --use_preluSE --network_weights_path ./savedmodels/Unet__SE_PRELU.pt --idtest $i
-
-
-    python -u train.py --network_arch unet --use_augs --use_SE --use_preluSE --boxdown --network_weights_path ./savedmodels/Unet_SE_PRELU_boxconv.pt --idtest $i
-    python -u test.py --network_arch unet  --use_SE --use_preluSE --boxdown --network_weights_path ./savedmodels/Unet_SE_PRELU_boxconv.pt --idtest $i
-
-
-    python -u train.py --network_arch unet --use_mini --use_augs --network_weights_path ./savedmodels/Unet_mini.pt --idtest $i
-    python -u test.py --network_arch unet --use_mini --network_weights_path ./savedmodels/Unet_mini.pt --idtest $i
-
-
-    python -u train.py --network_arch unet --use_mini --use_augs --boxdown --network_weights_path ./savedmodels/Unet_mini_boxconv.pt --idtest $i
-    python -u test.py --network_arch unet --use_mini  --boxdown --network_weights_path ./savedmodels/Unet_mini_boxconv.pt --idtest $i
-
-
-    python -u train.py --network_arch unet --use_mini --use_augs --use_SE --use_preluSE --network_weights_path ./savedmodels/Unet_mini_SE_Prelu.pt --idtest $i
-    python -u test.py --network_arch unet --use_mini --use_SE --use_preluSE --network_weights_path ./savedmodels/Unet_mini_SE_Prelu.pt --idtest $i
-
-
-    python -u train.py --network_arch unet --use_mini --use_augs --use_SE --use_preluSE --boxdown --network_weights_path ./savedmodels/Unet_mini_SE_PRELU_boxconv.pt --idtest $i
-    python -u test.py --network_arch unet --use_mini --use_SE --use_preluSE --boxdown --network_weights_path ./savedmodels/Unet_mini_SE_PRELU_boxconv.pt --idtest $i
-done
+# 
+# do
+#     rm ./savedmodels/*
+#     python -u train.py --network_arch unet --use_augs --network_weights_path ./savedmodels/Unet_normal.pt --idtest $i
+#     python -u test.py --network_arch unet --network_weights_path ./savedmodels/Unet_normal.pt --idtest $i
+# 
+#     python -u train.py --network_arch unet --use_augs --boxdown --network_weights_path ./savedmodels/Unet_normal_boxconv.pt --idtest $i
+#     python -u test.py --network_arch unet --boxdown --network_weights_path ./savedmodels/Unet_normal_boxconv.pt --idtest $i
+# 
+# 
+#     python -u train.py --network_arch unet --use_augs --use_SE --use_preluSE --network_weights_path ./savedmodels/Unet__SE_PRELU.pt --idtest $i
+#     python -u test.py --network_arch unet  --use_SE --use_preluSE --network_weights_path ./savedmodels/Unet__SE_PRELU.pt --idtest $i
+# 
+# 
+#     python -u train.py --network_arch unet --use_augs --use_SE --use_preluSE --boxdown --network_weights_path ./savedmodels/Unet_SE_PRELU_boxconv.pt --idtest $i
+#     python -u test.py --network_arch unet  --use_SE --use_preluSE --boxdown --network_weights_path ./savedmodels/Unet_SE_PRELU_boxconv.pt --idtest $i
+# 
+# 
+#     python -u train.py --network_arch unet --use_mini --use_augs --network_weights_path ./savedmodels/Unet_mini.pt --idtest $i
+#     python -u test.py --network_arch unet --use_mini --network_weights_path ./savedmodels/Unet_mini.pt --idtest $i
+# 
+# 
+#     python -u train.py --network_arch unet --use_mini --use_augs --boxdown --network_weights_path ./savedmodels/Unet_mini_boxconv.pt --idtest $i
+#     python -u test.py --network_arch unet --use_mini  --boxdown --network_weights_path ./savedmodels/Unet_mini_boxconv.pt --idtest $i
+# 
+# 
+#     python -u train.py --network_arch unet --use_mini --use_augs --use_SE --use_preluSE --network_weights_path ./savedmodels/Unet_mini_SE_Prelu.pt --idtest $i
+#     python -u test.py --network_arch unet --use_mini --use_SE --use_preluSE --network_weights_path ./savedmodels/Unet_mini_SE_Prelu.pt --idtest $i
+# 
+# 
+#     python -u train.py --network_arch unet --use_mini --use_augs --use_SE --use_preluSE --boxdown --network_weights_path ./savedmodels/Unet_mini_SE_PRELU_boxconv.pt --idtest $i
+#     python -u test.py --network_arch unet --use_mini --use_SE --use_preluSE --boxdown --network_weights_path ./savedmodels/Unet_mini_SE_PRELU_boxconv.pt --idtest $i
+# done
 
 
 # for i in 0 1 2 3 4
@@ -46,3 +46,41 @@ done
 #     python -u test.py --network_arch unet --boxdown --network_weights_path ./savedmodels/Unet_normal_boxconv.pt --idtest $i
 # done
 # 
+
+# for i in 0 1 2 3 4
+# 
+# do
+#     rm ./savedmodels/*
+#     python -u train.py --network_arch unet --use_augs --use_SE --network_weights_path ./savedmodels/Unet_normal_OnlySE.pt --idtest $i
+#     python -u test.py --network_arch unet  --use_SE --network_weights_path ./savedmodels/Unet_normal_OnlySE.pt --idtest $i
+#     
+#     
+#     python -u train.py --network_arch unet --use_augs --use_SE --boxdown --network_weights_path ./savedmodels/Unet_normal_OnlySE_boxconv.pt --idtest $i
+#     python -u test.py --network_arch unet  --use_SE --boxdown --network_weights_path ./savedmodels/Unet_normal_OnlySE_boxconv.pt --idtest $i
+#     
+#     
+#     
+#     python -u train.py --network_arch unet --use_mini --use_augs --use_SE --network_weights_path ./savedmodels/Unet_mini_OnlySE.pt --idtest $i
+#     python -u test.py --network_arch unet --use_mini --use_SE --network_weights_path ./savedmodels/Unet_mini_OnlySE.pt --idtest $i
+#     
+#     python -u train.py --network_arch unet --use_mini --use_augs --use_SE --boxdown --network_weights_path ./savedmodels/Unet_mini_OnlySE_boxdown.pt --idtest $i
+#     python -u test.py --network_arch unet --use_mini --use_SE --boxdown --network_weights_path ./savedmodels/Unet_mini_OnlySE_boxdown.pt --idtest $i
+#     
+# done
+
+for i in 0 1 2 3 4
+
+do
+    rm ./savedmodels/*
+    python -u train.py --network_arch resnet  --network_weights_path ./savedmodels/resnet_normal.pt --idtest $i
+    python -u test.py --network_arch resnet   --network_weights_path ./savedmodels/resnet_normal.pt --idtest $i
+    
+    
+    python -u train.py --network_arch resnet --boxdown  --network_weights_path ./savedmodels/resnet_boxconv.pt --idtest $i
+    python -u test.py --network_arch resnet --boxdown   --network_weights_path ./savedmodels/resnet_boxconv.pt --idtest $i
+    
+done
+
+
+
+
